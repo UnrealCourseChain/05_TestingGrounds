f5c299087c2e5b9699f2724c10c741bc1cc1b8a9
David Hendry <chain_default@hotmail.com>
2018-03-14 22:41:18 -0600

    285 Converting a Character BP to C++

--- Source/TestingGrounds/Player/FirstPersonCharacter.cpp
+++ Source/TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+ // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 #include "FirstPersonCharacter.h"
 #include "Camera/CameraComponent.h"
